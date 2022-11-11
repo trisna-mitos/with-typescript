55069f1e9e6c0afec54d1698b7afa4c055f90a8d
trisna hasanudin <trisna@trisnas-MacBook-Pro.local>
2022-11-11 16:03:26 +0700

    update dong

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function IndexPage () {
   const { data: session,status } = useSession()
+
   if(session){
     return (
     <>
